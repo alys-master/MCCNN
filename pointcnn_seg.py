4b3eeff9797a7ac848eca60af7eeb18f6374219a
alys-master <42170049+alys-master@users.noreply.github.com>
2021-04-14 18:50:56 +0800

    Update pointcnn_seg.py

--- pointcnn_seg.py
+++ pointcnn_seg.py
@@ -3,10 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import pointfly as pf
-from pointcnn import PointCNN
+from mccnn import MCCNN
 
 
-class Net(PointCNN):
+class Net(MCCNN):
     def __init__(self, points, features, is_training, setting):
         PointCNN.__init__(self, points, features, is_training, setting)
         self.logits = pf.dense(self.fc_layers[-1], setting.num_class, 'logits',
